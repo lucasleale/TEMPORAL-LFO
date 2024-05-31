5cc96d3e261ff4210602a85b9f2640a412f5a2c1
lucasleale <e.lucas.leal@gmail.com>
2024-05-31 01:08:18 -0300

    neopixel lib_dep

--- src/main.cpp
+++ src/main.cpp
@@ -29,8 +29,8 @@
 #define OLED_RESET -1
 #define SCREEN_ADDRESS 0x3C  // CHEQUEAR ADDRESS, PUEDE SER 0x3C o 0x3D
 
-#define COMPENSATION 1.  // para ajustar periodo
-#define LED_REFRESH 33      // 1000/33 30fps
+#define COMPENSATION 1.   // para ajustar periodo
+#define LED_REFRESH 33    // 1000/33 30fps
 #define LED_BRIGHTNESS 1  // 0. a 1.
 #define LED_CLOCK_IN 0
 #define LED_LFO1 1
@@ -701,10 +701,10 @@ void updateLfoLeds() {
   if (ledsFps > LED_REFRESH) {
     ledsFps = 0;
     // leds.setPixelColor(5, leds.Color(random(256), 100, 30));
-leds.setPixelColor(LED_LFO1, leds.Color(ledLfo1 * LED_BRIGHTNESS, 0, 0));
-leds.setPixelColor(LED_LFO2, leds.Color(ledLfo2 * LED_BRIGHTNESS, 0, 0));
-    //leds.setPixelColor(LED_LFO1, leds.Color(ledLfo1 * LED_BRIGHTNESS, ledLfo2 * 0.1, ledLfo2 * 0.2));
-    //leds.setPixelColor(LED_LFO2, leds.Color(ledLfo2 * LED_BRIGHTNESS, ledLfo1 * 0.1, ledLfo1 * 0.2));
+    leds.setPixelColor(LED_LFO1, leds.Color(ledLfo1 * LED_BRIGHTNESS, 0, 0));
+    leds.setPixelColor(LED_LFO2, leds.Color(ledLfo2 * LED_BRIGHTNESS, 0, 0));
+    // leds.setPixelColor(LED_LFO1, leds.Color(ledLfo1 * LED_BRIGHTNESS, ledLfo2 * 0.1, ledLfo2 * 0.2));
+    // leds.setPixelColor(LED_LFO2, leds.Color(ledLfo2 * LED_BRIGHTNESS, ledLfo1 * 0.1, ledLfo1 * 0.2));
     leds.show();
   }
 }
